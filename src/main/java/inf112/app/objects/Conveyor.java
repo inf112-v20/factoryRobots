3b5aecee3c65d8951be8bd4d9fd9a1c91220e1ae
selfjell <h.trondsen@hotmail.com>
2020-02-20 12:58:25 +0100

    add conveyor class and objectfactory

---
+++ src/main/java/inf112/app/objects/Conveyor.java
@@ -0,0 +1,11 @@
+package inf112.app.objects;
+
+public class Conveyor {
+    private Direction[] entries;
+    private Direction exit;
+
+    public Conveyor(){
+        entries = new Direction[2];
+
+    }
+}
--- src/main/java/inf112/app/objects/ObjectFactory.java
+++ src/main/java/inf112/app/objects/ObjectFactory.java
@@ -1,12 +1,14 @@
 package inf112.app.objects;
 
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+
 public class ObjectFactory {
     //private IDTranslator translator;
     public ObjectFactory(){
         //translator = new IDTranslator();
     }
 
-    public static IBoardElement generateObject(int id){
+    public static IBoardElement generateObject(int id, StaticTiledMapTile tile){
         ElemEnum name = translator.findNameFromId(id);
         switch(name){
             case ElemEnum.BELTX1: return null;
@@ -15,7 +17,7 @@ public class ObjectFactory {
             case ElemEnum.WALL: return null;
             case ElemEnum.LASER: return null;
             case ElemEnum.REPAIRSTATION: return null;
-            default throw new IllegalArgumentException("Something is wrong");
+            default: throw new IllegalArgumentException("Something is wrong");
         }
         return null;
     }

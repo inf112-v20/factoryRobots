fd0750eafa58e327bf9fb2eb2cd58d3ea4403124
selfjell <h.trondsen@hotmail.com>
2020-02-26 15:28:15 +0100

    add objectfactory tests

--- src/test/java/inf112/app/objects/IDTranslatorTest.java
+++ src/test/java/inf112/app/objects/IDTranslatorTest.java
@@ -1,12 +1,57 @@
 package inf112.app.objects;
 
+import org.junit.Before;
 import org.junit.Test;
+import inf112.app.objects.IDTranslator.ElemEnum;
 
 import static org.junit.Assert.*;
 
 public class IDTranslatorTest {
+    IDTranslator translator;
+
+    @Before
+    public void setUp() throws Exception {
+        translator = new IDTranslator();
+    }
 
     @Test
-    public void findNameFromId() {
+    public void findWallFromWallIdTest() {
+        assertEquals(ElemEnum.WALL,translator.findNameFromId(29));
     }
+
+    @Test
+    public void findConveyorFromConveyorIdTest() {
+        assertEquals(ElemEnum.BELTX2,translator.findNameFromId(18));
+    }
+
+    @Test
+    public void findFlagFromFlagIdTest() {
+        assertEquals(ElemEnum.FLAG,translator.findNameFromId(55));
+    }
+
+    @Test
+    public void findHoleFromHoleIdTest() {
+        assertEquals(ElemEnum.HOLE,translator.findNameFromId(6));
+    }
+
+    @Test
+    public void findCogFromCogIdTest() {
+        assertEquals(ElemEnum.COG,translator.findNameFromId(53));
+    }
+
+    @Test
+    public void findLaserFromLaserIdTest() {
+        assertEquals(ElemEnum.LASER,translator.findNameFromId(39));
+    }
+
+    @Test
+    public void findBoardFromBoardIdTest() {
+        assertEquals(ElemEnum.BOARD,translator.findNameFromId(5));
+    }
+
+    @Test
+    public void findRepairStationFromRepairStationIDTest() {
+        assertEquals(ElemEnum.REPAIRSTATION,translator.findNameFromId(7));
+    }
+
 }
--- src/test/java/inf112/app/objects/ObjectFactoryTest.java
+++ src/test/java/inf112/app/objects/ObjectFactoryTest.java
@@ -1,10 +1,19 @@
 package inf112.app.objects;
 
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ObjectFactoryTest {
+    TiledMapTile tile;
+    @Before
+    public void setUp() throws Exception {
+        TmxMapLoader loader = new TmxMapLoader();
+        ObjectFactory factory = new ObjectFactory();
+    }
 
     @Test
     public void generateObject() {

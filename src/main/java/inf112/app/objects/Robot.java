f4715cc60803d93ea7cc3abced335a0e30914d41
RagnhildKSteinsland <ragnhild98@live.no>
2020-02-27 15:03:41 +0100

    Commented on robot and conveyor class

--- src/main/java/inf112/app/objects/Robot.java
+++ src/main/java/inf112/app/objects/Robot.java
@@ -4,10 +4,19 @@ import com.badlogic.gdx.Game;
 import inf112.app.map.Map;
 import inf112.app.objects.Direction.Rotation;
 
+/**
+ * this class is a representation of the robots
+ * on the board
+ */
 public class Robot extends Game {
     private Map map;
     private Position pos;
 
+    /**
+     * constructor for the robot
+     * @param pos
+     * @param map
+     */
     public Robot(Position pos, Map map){
         this.pos = pos;
         this.map = map;
@@ -18,6 +27,10 @@ public class Robot extends Game {
 
     }
 
+    /**
+     * method to change the possition of the robot
+     * @param steps
+     */
     public void move(int steps){
         while(steps!=0){
             steps -= 1;
@@ -27,10 +40,17 @@ public class Robot extends Game {
         }
     }
 
+    /**
+     * method to change the direction of the robot
+     * @param r
+     */
     public void turn(Rotation r){
         pos.getDirection().turn(r);
     }
 
+    /**
+     * @return the position of the robot
+     */
     public Position getPos() {
         return pos;
     }

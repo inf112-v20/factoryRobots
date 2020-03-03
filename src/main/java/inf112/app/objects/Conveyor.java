bcd09eba44df9c82be98397ad8a50d6bbf1ccf4b
RagnhildKSteinsland <ragnhild98@live.no>
2020-03-03 16:31:37 +0100

    Implemented conveyor functionality

--- src/main/java/inf112/app/objects/Conveyor.java
+++ src/main/java/inf112/app/objects/Conveyor.java
@@ -8,6 +8,8 @@ public class Conveyor implements IBoardElement {
     private Direction[] entries;
     private Direction exit;
     private int speed;
+    private boolean rotate;
+    private Direction.Rotation rotation;
 
     /**
      * constructor for the conveyor belts
@@ -24,4 +26,25 @@ public class Conveyor implements IBoardElement {
         this.speed = speed;
 
     }
+
+    public Conveyor(int entry1, int entry2, int exit, int speed, boolean rotate, Direction.Rotation r){
+        entries = new Direction[2];
+        entries[0] = new Direction(entry1);
+        entries[1] = new Direction(entry2);
+        this.exit = new Direction(exit);
+        this.speed = speed;
+        this.rotate = rotate;
+        this.rotation = r;
+
+    }
+
+    public void conveyorAction(Player player){
+        if (rotate){
+            player.turn(rotation);
+            player.move(speed);
+        }else {
+            player.move(speed);
+        }
+    }
+
 }

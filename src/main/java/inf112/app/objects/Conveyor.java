f5d779d9ca2ddaac4be716085f210d67eee981d0
RagnhildKSteinsland <ragnhild98@live.no>
2020-03-04 20:30:26 +0100

    changes made conveyor

--- src/main/java/inf112/app/objects/Conveyor.java
+++ src/main/java/inf112/app/objects/Conveyor.java
@@ -1,5 +1,6 @@
 package inf112.app.objects;
 
+import inf112.app.objects.Direction.Rotation;
 /**
  * This class is a representation of the
  * Conveyor belts on the board
@@ -9,7 +10,7 @@ public class Conveyor implements IBoardElement {
     private Direction exit;
     private int speed;
     private boolean rotate;
-    private Direction.Rotation rotation;
+    private Rotation rotation;
 
     /**
      * constructor for the conveyor belts
@@ -27,7 +28,7 @@ public class Conveyor implements IBoardElement {
 
     }
 
-    public Conveyor(int entry1, int entry2, int exit, int speed, boolean rotate, Direction.Rotation r){
+    public Conveyor(int entry1, int entry2, int exit, int speed, boolean rotate, Rotation r){
         entries = new Direction[2];
         entries[0] = new Direction(entry1);
         entries[1] = new Direction(entry2);
@@ -38,10 +39,15 @@ public class Conveyor implements IBoardElement {
 
     }
 
-    public void conveyorAction(Player player){
+    /**
+     * implementation of action to be done when robot is on a conveyor belt
+     * @param player
+     */
+    @Override
+    public void doAction(Player player){
         if (rotate){
-            player.turn(rotation);
-            player.move(speed);
+            player.getCharacter().turn(rotation);
+            player.getCharacter().move(speed);
         }else {
             player.move(speed);
         }

a90f6c9d44d6503ae40581a8324383b0b7eca44f
haa037 <halvor.aalen@student.uib.no>
2020-03-05 17:20:59 +0100

    Add icon for menu

--- src/main/java/inf112/app/game/MainMenuScreen.java
+++ src/main/java/inf112/app/game/MainMenuScreen.java
@@ -2,14 +2,19 @@ package inf112.app.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class MainMenuScreen implements Screen {
 
     final RoboRally game;
 
     OrthographicCamera camera;
+    Skin skin = new Skin();
+    TextureRegion test;
 
     public MainMenuScreen(final RoboRally game) {
         this.game = game;
@@ -21,20 +26,33 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void show() {
+        //test = new TextureRegion( new Texture(Gdx.files.internal("game-menu.png")));
 
     }
 
     @Override
     public void render(float v) {
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Color x = Color.LIGHT_GRAY;
+        Gdx.gl.glClearColor(x.r, x.g, x.b, x.a );
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        game.font.draw(game.batch, "Welcome to Robo Rally!!! ", 350, 150);
-        game.font.draw(game.batch, "Tap anywhere to begin!", 350, 100);
+        /*FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(""));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 18;
+        parameter.characters = FreeTypeFontGenerator.DEFAULT_CHARS;
+        BitmapFont font = generator.generateFont(parameter);
+        generator.dispose();
+        font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+        font.draw(game.batch, "Robo Rally", 25, 160);*/
+        //game.batch.draw(test,50,-200);
+
+        //game.font.draw(game.batch, "Welcome to Robo Rally!!! ", 500, 500);
+        //game.font.draw(game.batch, "Tap anywhere to begin!", 500, 400);
+
         game.batch.end();
 
         if (Gdx.input.isTouched()) {
@@ -67,7 +85,6 @@ public class MainMenuScreen implements Screen {
     public void dispose() {
 
     }
-
 
     //...Rest of class omitted for succinctness.
 

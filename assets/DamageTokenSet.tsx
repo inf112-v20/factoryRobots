b979b2187f78afb103fd53cc3e48efe8ebebe5eb
haa037 <halvor.aalen@student.uib.no>
2020-04-01 13:12:24 +0200

    Add assets

---
+++ assets/DamageTokenSet.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="DamageTokenSet" tilewidth="400" tileheight="600" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="400" height="600" source="damage-token-grey.png"/>
+ </tile>
+ <tile id="2">
+  <image width="400" height="600" source="damage-token-one.png"/>
+ </tile>
+ <tile id="3">
+  <image width="400" height="600" source="damage-token-both.png"/>
+ </tile>
+</tileset>

34be337f608fc6712dfb5625e6ad13d397ec78d9
selfjell <h.trondsen@hotmail.com>
2020-04-29 15:27:56 +0200

    fix soundbutton having different buttondown icon

--- src/main/java/inf112/app/game/GameButtonActor.java
+++ src/main/java/inf112/app/game/GameButtonActor.java
@@ -22,7 +22,7 @@ public class GameButtonActor extends ButtonActor {
         this.type = type;
         int index = "powerdown".equals(type) ? 1 : 3;
         TiledMapTileLayer buttons = CardUI.getInstance().getUiButtons();
-        buttonDown = buttons.getCell(index,0);
+        buttonDown = "sound".equals(type) ? buttonUp : buttons.getCell(index,0);
         this.x = x;
         this.y = y;
         this.stage = stage;
@@ -60,6 +60,7 @@ public class GameButtonActor extends ButtonActor {
             } else if ("powerdown".equals(type)) {
                 stage.getGame().getPlayer().getCharacter().setPowerDownNextRound(true);
             } else if ("sound".equals(type)){
+                pushable = true;
                 RoboRally game = stage.getGame();
                 if (game.backgroundMusic.isPlaying()) {
                     game.backgroundMusic.pause();
